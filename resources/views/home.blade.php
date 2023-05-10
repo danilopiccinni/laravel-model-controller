b3e5f7aa69741c5d813c19dae05528b9bffe2257
danilopiccinni <danilopiccinni@gmail.com>
2023-05-10 14:45:11 +0200

    stilizazzione in tabella e card(bonus)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,14 +17,61 @@
 
 <body>
 
-    <ul>
-        @foreach ($movies as $movie)
-            <li>
-                {{ $movie->title }}
-            </li>   
-        @endforeach
-    </ul>
+    <section class="sezione sezione-1">
 
+        <h2 class="titolo" >Tutti i film in formato tabella(database)</h2>
+
+        <div class="container">
+            <table class="table table-dark table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">id</th>
+                        <th scope="col">title</th>
+                        <th scope="col">original_title</th>
+                        <th scope="col">nationality</th>
+                        <th scope="col">date</th>
+                        <th scope="col">vote</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($movies as $movie)
+                    <tr>
+                        <th scope="row">{{ $movie->id }}</th>
+                        <td>{{ $movie->title }}</td>
+                        <td>{{ $movie->original_title }}</td>
+                        <td>{{ $movie->nationality }}</td>
+                        <td>{{ $movie->date }}</td>
+                        <td>{{ $movie->vote }}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </section>
+
+
+    <section class="sezione sezione-2">
+
+        <h2>Tutti i film in cards</h2>
+    
+        <div class="container contenitore-card">
+
+            @foreach ($movies as $movie)
+            <div class="card col-2">
+
+                <strong>{{ $movie->title }}</strong>
+                <em>{{ $movie->original_title }}</em>
+                <small>{{ $movie->nationality }}</small>
+                <small>{{ $movie->date }}</small>
+                <small>{{ $movie->vote }}</small>
+
+            </div>
+            @endforeach
+
+        </div>
+            
+    </section>
+        
 </body>
 
 </html>
